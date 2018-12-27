0ddfbecb5d911b2d0879fca83c06b70a477df9de
Dalacorte <victordalacorte@hotmail.com>
2018-12-26 23:45:43 -0200

    Java

--- java/alura/produtos/src/main/java/br/com/alura/maven/App.java
+++ java/alura/produtos/src/main/java/br/com/alura/maven/App.java
@@ -6,9 +6,9 @@ package br.com.alura.maven;
  */
 public class App {
 	public static void main(String[] args) {
-
+		
 		Produto produto = new Produto("Bala juquinha sabor tangerina", 0.15);
-
-		System.out.println("A bala que eu gosto é: " + produto.getNome() + " e custa " + produto.getPreco());
+		
+		System.out.println("A bala que eu gosto é: "+ produto.getNome() + " e custa " + produto.getPreco());
 	}
 }
