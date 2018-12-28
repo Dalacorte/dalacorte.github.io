dd2281b11025fd3c67fa31e19c2d9e0f26b91ed7
Dalacorte <victordalacorte@hotmail.com>
2018-12-28 21:54:13 -0200

    Ruby

--- ruby/maior-ou-menor.rb
+++ ruby/maior-ou-menor.rb
@@ -1,24 +1,56 @@
-puts "Bem-voindo ao jogo da adivinhação"
-puts "Qual é o seu nome?"
-nome = gets
+# ruby is fun
 
-puts
-puts 
-puts
+=begin
+	27
+	Dezembro
+	2018
+=end
 
-puts "Começaremos o jogo para voce, " + nome
+def da_boas_vindas
+	puts "Bem-vindo ao jogo da adivinhação!"
+	puts "Qual é o seu nome?"
+	nome = gets
+	puts "\n\n\n"
+	puts "Começaremos o jogo para voce, " + nome
+end
 
-puts "Escolhendo um número entre 0 e 200..."
-numero_secreto = 175
-puts "Escolhido... Que tal adivinhar o nosso número secreto?!"
+def sorteia_numero_secreto
+	puts "Escolhendo um número entre 0 e 200..."
+	sorteado = 175
+	puts "Escolhido... Que tal adivinhar o nosso número secreto?!"
+	return sorteado
+end
 
-puts
-puts
-puts
+def pede_um_numero(tentativa, limite_de_tentativas)
+	puts "\n\n\n"
+	puts "Tentativa " + tentativa.to_s + " de " + limite_de_tentativas.to_s
+	puts "Entre com o número"
+	chute = gets
+	return chute
+end
 
-puts "Tentativa 1";
-puts "Entre com o numero"
-chute = gets
+def verifica_se_acertou(numero_secreto, chute)
+	acertou = numero_secreto == chute.to_i
+	if acertou
+		puts "Acertou!"
+		return true
+	end
+	puts "Errou!"
+	maior = numero_secreto > chute.to_i
+	if maior
+		puts "O número secreto é maior!"
+	else
+		puts "O número secreto é menor!"
+	end
+	false
+end
 
-puts"Será que acertou?! Você chutou: "+ chute
-puts chute.to_i == numero_secreto
+da_boas_vindas
+numero_secreto = sorteia_numero_secreto
+
+limite_de_tentativas = 5
+
+for tentativa in 1..limite_de_tentativas
+	chute = pede_um_numero tentativa, limite_de_tentativas
+	break if verifica_se_acertou numero_secreto, chute 
+end 
--- ruby/testes.rb
+++ ruby/testes.rb
@@ -9,3 +9,6 @@ puts 175 > 175
 puts 175 < 175
 puts 175 >= 175
 puts 175 <= 175
+for dedos in 1..3
+	puts "Contando " + dedos.to_s
+end
